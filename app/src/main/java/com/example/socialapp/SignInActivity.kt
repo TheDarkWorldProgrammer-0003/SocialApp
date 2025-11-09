c7b4a71912d0dcc7f128bd1520e3ec20fe5b7acd
Shrey Vashisth <143152184+TheDarkWorldProgrammer-0003@users.noreply.github.com>
2025-11-09 19:15:39 +0530

    .
    .

--- app/src/main/java/com/example/socialapp/SignInActivity.kt
+++ app/src/main/java/com/example/socialapp/SignInActivity.kt
@@ -1,16 +1,47 @@
 package com.example.socialapp
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.credentials.GetCredentialRequest
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
 
 class SignInActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN: Int = 123
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var signInButton: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var progressBar : ProgressBar
+
+    private lateinit var  binding : ActivitySignInBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_in)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -18,23 +49,78 @@ class SignInActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         // Instantiate a Google sign-in request
-        val googleIdOption = GetGoogleIdOption.Builder()
-            // Your server's client ID, not your Android client ID.
-            .setServerClientId(getString(R.string.default_web_client_id))
-            // Only show accounts previously used to sign in.
-            .setFilterByAuthorizedAccounts(true)
+        val googleIdOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
             .build()
 
-// Create the Credential Manager request
-        val request = GetCredentialRequest.Builder()
-            .addCredentialOption(googleIdOption)
-            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleIdOption)
 
+        auth = FirebaseAuth.getInstance()
+        binding.signInButton.setOnClickListener { signIn() }
 
 
     }
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            handleSignInResult(task)
+
+
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+            firebaseAuthWithGoogle(account.idToken!!)
+        } catch (e: ApiException) {
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+        }
+    }
+
+
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+
+        binding.signInButton.visibility = View.GONE
+        binding.progressBar.visibility = View.VISIBLE
+
+        GlobalScope.launch(Dispatchers.IO) // Global scope is used to launch coroutines outside of an activity or fragment's lifecycle.
+        {
+            val auth = auth.signInWithCredential(credential).await()
+            val firebaseUser = auth.user
+            withContext(Dispatchers.Main)// withcontext is used to switch the context of the coroutine to the main thread.
+            {
+                updateUI(firebaseUser)
+            }
+        }
+
+    }
+
+    private fun updateUI(firebaseUser: FirebaseUser?) {
+        if (firebaseUser != null) {
+            val mainActivity = Intent(this, MainActivity::class.java)
+            startActivity(mainActivity)
+            finish()
+
+        }
+        else{
+            signInButton.visibility = View.VISIBLE
+
+            progressBar.visibility = View.GONE
+
+        }
+    }
 
 }
